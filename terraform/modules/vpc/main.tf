0e4b17b2730037c02f2408e5b044b2049666acf2
Oleg N <n.ol@mail.ru>
2020-01-30 15:52:37 +0500

    Ex.12 Перед выполнением задания *

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -8,3 +8,15 @@ resource "google_compute_firewall" "firewall_ssh" {
   }
  source_ranges = var.source_ranges
 }
+
+resource "google_compute_firewall" "firewall_web" {
+  name    = "allow-web"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+source_tags = ["web"] 
+source_ranges = var.source_ranges
+}
