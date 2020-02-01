81deef2f40f57f1b13d396a5538290c2a3fe3b62
Oleg N <n.ol@mail.ru>
2020-02-01 15:56:54 +0500

    Ex.12 fix for travis

--- travis/travis_install_pkg.sh
+++ travis/travis_install_pkg.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#cd /tmp
+cd /tmp
 curl -L -O https://releases.hashicorp.com/packer/1.5.1/packer_1.5.1_linux_amd64.zip
 curl -L -O https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip
 curl -L -O https://github.com/terraform-linters/tflint/releases/download/v0.13.4/tflint_linux_amd64.zip
 
 ls -l
-#echo "install pkg"
-#sudo unzip -o -d /usr/local/bin/ "*.zip"
+echo "install pkg"
+sudo unzip -o -d /usr/local/bin/ "*.zip"
 #sudo unzip -o -d /usr/local/bin/ packer_1.5.1_linux_amd64.zip
 #sudo unzip -o -d /usr/local/bin/ terraform_0.12.9_linux_amd64.zip
 #sudo unzip -o -d /usr/local/bin/ tflint_linux_amd64.zip
-#echo "remove pkg"
-#rm *.zip
-#ls -l
-#echo "check pkg"
+echo "remove pkg"
+rm *.zip
+ls -l
+echo "check pkg"
 
-#echo "packer: $(packer --version)"
-#terraform --version
-#tflint -v
+echo "packer: $(packer --version)"
+terraform --version
+tflint -v
 
-#pip install --user --upgrade pip
-#pip install --user ansible ansible-lint
-
+pip install --user --upgrade pip
+pip install --user ansible ansible-lint
+cd ~
