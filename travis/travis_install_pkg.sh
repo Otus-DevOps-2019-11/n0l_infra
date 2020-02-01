b066c92052b23e224e1b0678e8492761142a1fa9
Oleg N <n.ol@mail.ru>
2020-02-01 15:05:52 +0500

    Ex.12 fix for travis

--- travis/travis_install_pkg.sh
+++ travis/travis_install_pkg.sh
@@ -5,7 +5,11 @@ curl -L -O https://releases.hashicorp.com/packer/1.5.1/packer_1.5.1_linux_amd64.
 curl -L -O https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip
 curl -L -O https://github.com/terraform-linters/tflint/releases/download/v0.13.4/tflint_linux_amd64.zip
 
-sudo unzip -o -d /usr/local/bin/ "*.zip"
+#sudo unzip -o -d /usr/local/bin/ "*.zip"
+sudo unzip -o -d /usr/local/bin/ packer_1.5.1_linux_amd64.zip
+sudo unzip -o -d /usr/local/bin/ terraform_0.12.9_linux_amd64.zip
+sudo unzip -o -d /usr/local/bin/ tflint_linux_amd64.zip
+
 rm *.zip
 
 echo "packer: $(packer --version)"
